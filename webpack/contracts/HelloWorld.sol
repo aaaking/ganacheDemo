0b12ff7bd37df94d05be619bd438daa52dc26309
zhouzhihui <zhouzhihui@htgames.cn>
2018-04-13 16:01:21 +0800

    code

--- webpack/contracts/HelloWorld.sol
+++ webpack/contracts/HelloWorld.sol
@@ -2,11 +2,15 @@ pragma solidity ^0.4.17;
 
 contract HelloWorld {
     function say() public pure returns (string) {
-        return "Hellow World function say()";
+        return "Hellow World function";
     }
 
     //
     function print(string name) public pure returns (string) {
         return name;
+    }
+
+    function getBalance() returns (uint){
+        return this.balance;
     }
 }
