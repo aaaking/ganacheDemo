50844036e8c9556455f0ee9726b2465073fdb031
zhouzhihui <zhouzhihui@htgames.cn>
2018-04-13 09:50:11 +0800

    delete MetaCoin.sol

--- delete2/test/TestMetacoin.sol
+++ delete2/test/TestMetacoin.sol
@@ -2,24 +2,24 @@ pragma solidity ^0.4.2;
 
 import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
-import "../contracts/MetaCoin.sol";
+import "../contracts/SanGuoSha.sol";
 
 contract TestMetacoin {
 
   function testInitialBalanceUsingDeployedContract() public {
-    MetaCoin meta = MetaCoin(DeployedAddresses.MetaCoin());
+    SanGuoSha meta = SanGuoSha(DeployedAddresses.SanGuoSha());
 
     uint expected = 10000;
 
-    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
+    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 SanGuoSha initially");
   }
 
-  function testInitialBalanceWithNewMetaCoin() public {
-    MetaCoin meta = new MetaCoin();
+  function testInitialBalanceWithNewSanGuoSha() public {
+    SanGuoSha meta = new SanGuoSha();
 
     uint expected = 10000;
 
-    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 MetaCoin initially");
+    Assert.equal(meta.getBalance(tx.origin), expected, "Owner should have 10000 SanGuoSha initially");
   }
 
 }
