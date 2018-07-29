2c5fdecd1b43a5397856ea3018e701b8a81d4694
周智慧 <zhouzhihui@dobest.com>
2018-07-30 02:26:30 +0800

    file refactor

--- webpack/migrations/1_initial_migration.js
+++
@@ -1,6 +0,0 @@
-var Migrations = artifacts.require("./Migrations.sol");
-// var HelloWorld = artifacts.require("./HelloWorld.sol")
-module.exports = function (deployer) {
-    deployer.deploy(Migrations);
-    // deployer.deploy(HelloWorld);
-};
